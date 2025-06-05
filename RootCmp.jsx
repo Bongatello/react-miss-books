7dd4c12c97dbb814179f47d8d6727246e5618e38
Timur H. <bongatelloninja@gmail.com>
2025-06-05 14:30:05 +0300

    first commit, just the skeleton with util serivce and storage service

---
+++ RootCmp.jsx
@@ -0,0 +1,33 @@
+const Router = ReactRouterDOM.HashRouter
+const { Routes, Route, Navigate } = ReactRouterDOM
+
+
+import { AppHeader } from "./cmps/AppHeader.jsx"
+import { NotFound } from "./cmps/NotFound.jsx"
+import { AboutUs } from "./pages/AboutUs.jsx"
+import { BookIndex } from "./pages/BookIndex.jsx"
+import { BookEdit } from "./pages/BookEdit.jsx"
+import { Home } from "./pages/HomePage.jsx"
+import { BookDetails } from "./pages/BookDetails.jsx"
+
+
+export function App() {
+
+    return (
+        <Router>
+            <section className="app">
+                <AppHeader />
+
+                <main className="main-layout">
+                    <Routes>
+                        <Route path="/" element={<Navigate to="/home" />} />
+                        <Route path="/home" element={<Home />} />
+                        <Route path="/about" element={<AboutUs />} />
+                        <Route path="/book" element={<BookIndex />} />
+                        <Route path="*" element={<NotFound />} />
+                    </Routes>
+                </main>
+            </section>
+        </Router>
+    )
+} 
