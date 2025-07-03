6d9b8f13c1650c8d95edfb54e2f853ee1be3ba5e
Timur H. <bongatelloninja@gmail.com>
2025-07-03 17:39:31 +0300

    -Changed shown items in BookPreview -Changed shown items in BookDetails -Added logic to publishedDate and pageCount -Added logic through classNames in price coloring -Some more minor changes

--- cmps/BookPreview.jsx
+++ cmps/BookPreview.jsx
@@ -4,7 +4,7 @@ export function BookPreview({book}) {
     return(
         <div className="book-preview">
             <h2>Title: {book.title}</h2>
-            <h4>Description: {book.description}</h4>
+            <img src={book.thumbnail} alt={'image could not be retrieved: ' + book.thumbnail}/>
             <h5>Price: {book.listPrice.amount} {book.listPrice.currencyCode}</h5>
             <h5>ID: {book.id}</h5>
             <h5>{book.listPrice.isOnSale ? 'in stock' : 'out of stock'}</h5>
--- pages/BookDetails.jsx
+++ pages/BookDetails.jsx
@@ -20,15 +20,32 @@ export function BookDetails(){
 
     }
 
+    function readingType(pageCount){
+        if (pageCount<100) return <h3 style={{color: 'DarkGreen'}}>Light Reading</h3> //will stop here if its under 100
+        else if (pageCount<500) return <h3 style={{color: 'DarkGoldenRod'}}>Descent Reading</h3> //if its lower than 100, should've stopped at the first if
+        else return <h3 style={{color: 'DarkRed'}}>Serious Reading</h3>
+    }
+
+    function vintageNew(publishedDate){
+        if (publishedDate<2015) return <h3 style={{color: 'DarkRed'}}>Vintage</h3>
+        else return <h3 style={{color: 'DarkGreen'}}>New</h3>
+    }
+
     if(!book) return <div>Loading Book...</div>
     return(
         <section className="book-details">
-            <h2>Title: {book.title}</h2>
-            <h2>Description: {book.description}</h2>
-            <h2></h2>
-            <h2>ID: {book.id}</h2>
-            <h2>Price: {book.listPrice.amount} {book.listPrice.currencyCode}</h2>
-            <h2>{book.listPrice.isOnSale ? 'in stock' : 'out of stock'}</h2>
+            <h1>Title: {book.title}</h1>
+            <img src={book.thumbnail} alt={'image could not be retrieved' + book.thumbnail}/>
+            <div style={{ display: 'flex', alignItems: 'center'}}> <h3>Subtitle:</h3><p>{book.subtitle}</p> </div>
+            <h3>Authors: {book.authors}</h3>
+            {vintageNew(book.publishedDate)}
+            {readingType(book.pageCount)}
+
+            <div style={{ display: 'flex', alignItems: 'center'}}> <h3>Description:</h3><p>{book.description}</p> </div>
+
+            <h3>ID: {book.id}</h3>
+            <h3 className = {book.listPrice.amount > 150 ? 'expensive' : book.listPrice.amount < 20 ? 'cheap' : ''}>Price: {book.listPrice.amount} {book.listPrice.currencyCode}</h3>
+            <h3>{book.listPrice.isOnSale ? 'in stock' : 'out of stock'}</h3>
             
         </section>
     )
